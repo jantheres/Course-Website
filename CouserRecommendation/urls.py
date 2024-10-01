7ea5b988a063b071e1c82af710a7ae5293d48c55
jantheres <jantheres31@gmail.com>
2024-10-01 12:35:26 +0530

    first commit

--- CouserRecommendation/urls.py
+++ CouserRecommendation/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
 
-from CouserRecommendation.CouserRecommendation import settings
+from CouserRecommendation import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
